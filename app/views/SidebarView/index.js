892fdb3ba5cdb2633fd00559f9d4c6f1546990cc
Seth Yates <95386742+SethY8s@users.noreply.github.com>
2023-11-03 00:44:38 +0700

    added email composer

--- app/views/SidebarView/index.js
+++ app/views/SidebarView/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, {useEffect} from 'react';
 import {
   Image,
   ScrollView,
@@ -8,26 +8,28 @@ import {
   Linking,
   SafeAreaView,
   Pressable,
-} from 'react-native'
-import { connect } from 'react-redux'
+  Button,
+} from 'react-native';
+import {connect} from 'react-redux';
 
-import { COLOR_YELLOW, themes } from '../../constants/colors'
-import StatusBar from '../../containers/StatusBar'
-import { withTheme } from '../../theme'
-import styles from './styles'
-import images from '../../assets/images'
-import SidebarItem from './SidebarItem'
-import scrollPersistTaps from '../../utils/scrollPersistTaps'
-import { logout as logoutAction } from '../../actions/login'
-import { showConfirmationAlert } from '../../lib/info'
-import { GradientHeader } from '../../containers/GradientHeader'
-import I18n from '../../i18n'
-import { SITE_SHOP_URL } from '../../constants/app'
-import { VectorIcon } from '../../containers/VectorIcon'
-import OptionCardBtn from '../../containers/OptionCardBtn'
+import {COLOR_YELLOW, themes} from '../../constants/colors';
+import StatusBar from '../../containers/StatusBar';
+import {withTheme} from '../../theme';
+import styles from './styles';
+import images from '../../assets/images';
+import SidebarItem from './SidebarItem';
+import scrollPersistTaps from '../../utils/scrollPersistTaps';
+import {logout as logoutAction} from '../../actions/login';
+import {showConfirmationAlert} from '../../lib/info';
+import {GradientHeader} from '../../containers/GradientHeader';
+import I18n from '../../i18n';
+import {SITE_SHOP_URL} from '../../constants/app';
+import {VectorIcon} from '../../containers/VectorIcon';
+import OptionCardBtn from '../../containers/OptionCardBtn';
+import {openComposer, openInbox} from 'react-native-email-link';
 
-const SidebarView = (props) => {
-  const { user, theme, navigation } = props
+const SidebarView = props => {
+  const {user, theme, navigation} = props;
   const menus = [
     {
       id: 'shop',
@@ -64,56 +66,81 @@ const SidebarView = (props) => {
       route: 'Privacy',
       routes: ['HelpAndSupport'],
     },
-  ]
+  ];
+
+  // <Button
+  //   isLoading={isLoading}
+  //   onPress={() => {
+  //     openInbox({
+  //       message: 'Choose App',
+  //       cancelLabel: 'Go back',
+  //     });
+  //   }}>
+  //   Open Email
+  // </Button>;
 
   useEffect(() => {
     navigation.setOptions({
       title: 'VIP Billionaires',
       headerBackground: () => <GradientHeader />,
-    })
-  }, [])
+    });
+  }, []);
 
-  const onClick = item => {
+  const onClick = async item => {
     switch (item.id) {
       // case 'terms_of_use':
       //   return onNavigate('About', { type: 0 })
       case 'privacy_and_settings':
-        return onNavigate('MenuStack')
+        return onNavigate('MenuStack');
       // case 'eula':
       //   return onNavigate('About', { type: 2 })
       case 'shop':
-        return Linking.openURL(SITE_SHOP_URL)
+        return Linking.openURL(SITE_SHOP_URL);
       case 'help_and_support':
-        return onNavigate('HelpAndSupport')
+        // return onNavigate('HelpAndSupport')
+
+        console.log('fired');
+
+        const thing = await openComposer({
+          to: 'support@example.com',
+          subject: 'I have a question',
+          body: 'Hi, can you help me with...',
+        });
+
+        console.log('after await');
+        return thing;
+
+      // return console.log('help and support');
+
       case 'MyConnections':
-        return onNavigate('MyConnections')
+        return onNavigate('MyConnections');
       case 'vip_members':
-        return onNavigate('')
+        return onNavigate('');
       default:
-        onNavigate(item.route, { type: item.init })
+        onNavigate(item.route, {type: item.init});
     }
-  }
+  };
 
   const onNavigate = (routeName, params) => {
-    const { navigation } = props
-    navigation.navigate(routeName, params)
-  }
+    const {navigation} = props;
+    navigation.navigate(routeName, params);
+  };
 
   const onLogOut = () => {
-    const { logout } = props
+    const {logout} = props;
     showConfirmationAlert({
       title: I18n.t('Logout'),
       message: I18n.t('are_you_sure_to_log_out'),
       callToAction: I18n.t('Confirm'),
       onPress: () => {
         if (global.unSubscribeRoom) {
-          global.unSubscribeRoom()
-          global.unSubscribeRoom = undefined
+          global.unSubscribeRoom();
+          global.unSubscribeRoom = undefined;
         }
-        logout()
+        logout();
       },
-    })
-  }
+    });
+  };
 
   return (
     <SafeAreaView
@@ -126,23 +153,23 @@ const SidebarView = (props) => {
       <View style={styles.headerContainer}>
         <View style={styles.profileInnerContainer}>
           <Image
-            source={user.avatar ? { uri: user.avatar } : images.default_avatar}
+            source={user.avatar ? {uri: user.avatar} : images.default_avatar}
             style={styles.avatar}
           />
-          <View style={{ marginLeft: 12 }}>
+          <View style={{marginLeft: 12}}>
             <Text
-              style={[
-                styles.profileName,
-                { color: themes[theme].titleColor },
-              ]}>
+              style={[styles.profileName, {color: themes[theme].titleColor}]}>
               {user.displayName}
             </Text>
-            <Text style={[styles.roleName, { color: COLOR_YELLOW }]}>
+            <Text style={[styles.roleName, {color: COLOR_YELLOW}]}>
               View Profile
             </Text>
           </View>
         </View>
-        <Pressable onPress={() => navigation.closeDrawer()} style={styles.closeIconAndText}>
+
+        <Pressable
+          onPress={() => navigation.closeDrawer()}
+          style={styles.closeIconAndText}>
           <VectorIcon
             type="AntDesign"
             name="close"
@@ -150,7 +177,7 @@ const SidebarView = (props) => {
             color={themes[theme].textColor}
             style={styles.closeIcon}
           />
-          <Text style={[{ color: themes[theme].textColor }]}>Clear</Text>
+          <Text style={[{color: themes[theme].textColor}]}>Clear</Text>
         </Pressable>
       </View>
       <ScrollView
@@ -161,7 +188,7 @@ const SidebarView = (props) => {
         }}
         {...scrollPersistTaps}>
         <OptionCardBtn
-          subTextColor={{ color: COLOR_YELLOW }}
+          subTextColor={{color: COLOR_YELLOW}}
           image={images.reward_badge}
           title="Premium Subscription"
           smallText="Upgrade plan"
@@ -173,7 +200,20 @@ const SidebarView = (props) => {
           rightIcon
           rightIconName="share"
         />
-        <Text style={[styles.menuText, { color: themes[theme].titleColor }]}>Menu</Text>
+
+        <Button
+          title="dwad"
+          onPress={() => {
+            openComposer({
+              to: 'support@example.com',
+              subject: 'I have a question',
+              body: 'Hi, can you help me with...',
+            });
+          }}
+        />
+        <Text style={[styles.menuText, {color: themes[theme].titleColor}]}>
+          Menu
+        </Text>
         {menus.map(m => (
           <SidebarItem
             key={m.id}
@@ -184,7 +224,7 @@ const SidebarView = (props) => {
                 name={m.icon}
                 type={'MaterialCommunityIcons'}
                 size={20}
-                style={{ color: themes[theme].textColor }}
+                style={{color: themes[theme].textColor}}
               />
             }
             hasRight
@@ -196,43 +236,61 @@ const SidebarView = (props) => {
       </ScrollView>
       <TouchableOpacity
         onPress={onLogOut}
-        style={[styles.logoutBtn, { backgroundColor: themes[theme].buttonBackground }]}>
+        style={[
+          styles.logoutBtn,
+          {backgroundColor: themes[theme].buttonBackground},
+        ]}>
         <VectorIcon
           name={'logout-variant'}
           type={'MaterialCommunityIcons'}
           size={24}
-          style={{ color: themes[theme].textColor }}
+          style={{color: themes[theme].textColor}}
         />
         <Text
-          style={[styles.logoutText, { color: themes[theme].activeTintColor }]}>
+          style={[styles.logoutText, {color: themes[theme].activeTintColor}]}>
           {I18n.t('Logout').toUpperCase()}
         </Text>
       </TouchableOpacity>
       <View style={styles.bottomView}>
         <View style={styles.privacyTermsEulaContainer}>
-          <Text style={[styles.text, { color: themes[theme].textColor }]} onPress={() => {}}>Privacy policy</Text>
-          <Text style={[{ color: themes[theme].textColor }]}>.</Text>
-          <Text style={[styles.text, { color: themes[theme].textColor }]} onPress={() => {}}>
+          <Text
+            style={[styles.text, {color: themes[theme].textColor}]}
+            onPress={() => {}}>
+            Privacy policy
+          </Text>
+          <Text style={[{color: themes[theme].textColor}]}>.</Text>
+          <Text
+            style={[styles.text, {color: themes[theme].textColor}]}
+            onPress={() => {}}>
             Terms of services
           </Text>
-          <Text style={[{ color: themes[theme].textColor }]}>.</Text>
-          <Text style={[styles.text, { color: themes[theme].textColor }]} onPress={() => {}}>Eula</Text>
+          <Text style={[{color: themes[theme].textColor}]}>.</Text>
+          <Text
+            style={[styles.text, {color: themes[theme].textColor}]}
+            onPress={() => {}}>
+            Eula
+          </Text>
         </View>
         <View style={styles.languageContainer}>
           <Image source={images.en_language} />
-          <Text style={[styles.languageText, { color: themes[theme].textColor }]}>English (US)</Text>
+          <Text style={[styles.languageText, {color: themes[theme].textColor}]}>
+            English (US)
+          </Text>
         </View>
       </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
 const mapStateToProps = state => ({
   user: state.login.user,
-})
+});
 
 const mapDispatchToProps = dispatch => ({
   logout: params => dispatch(logoutAction(params)),
-})
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(withTheme(SidebarView))
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(withTheme(SidebarView));
